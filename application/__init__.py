c381ba1d25e859005015a317c0c055c31c3e94c3
AIR16019 <regis.van.moorleghem@atlascopco.com>
2023-12-15 14:58:34 +0100

    changed

--- application/__init__.py
+++ application/__init__.py
@@ -3,8 +3,9 @@ import connexion
 
 
 def create_app(config_name):
-    app = connexion.FlaskApp(__name__)
-    app.add_api("openapi.yaml")
+    connex_app = connexion.App(__name__)
+    connex_app.add_api('openapi.yaml')
+    app = connex_app.app  # the Flask app
     app.config.from_object(config_name)
 
     with app.app_context():
